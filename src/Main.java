cd6d495fec47f39dfbddecac36c214801ece7151
sparysiewicz <sparysiewicz@gmial.com>
2019-02-10 12:35:28 +0100

    Loop

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 import org.omg.PortableInterceptor.ServerRequestInfo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("please write bat's name");
-        String name = s.next();
-        System.out.println("you wrote: " + name);
-        System.out.println("please enter bat price per day");
-        Double price = s.nextDouble();
-        System.out.println("you enter: " + price);
-        Bat myBat = new Bat(price, name);
-        System.out.println(myBat);
 
+        List<Bat> bats = new ArrayList<>();
+        for (int i = 1; i < 4; i += 1) {
+            Scanner s = new Scanner(System.in);
+            System.out.println("please write bat's name");
+            String name = s.next();
+            System.out.println("you wrote: " + name);
+            System.out.println("please enter bat price per day");
+            Double price = s.nextDouble();
+            System.out.println("you enter: " + price);
+            Bat myBat = new Bat(price, name);
+            bats.add(myBat);
+        }
+        System.out.println(bats);
     }
 }
